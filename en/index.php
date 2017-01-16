0044c61141bcd659d18156006019fdd2022c5257
Grichan <vladgrichan@gmail.com>
2017-01-16 20:52:05 +0200

    Overall Design Change + images found

--- en/index.php
+++ en/index.php
@@ -7,46 +7,49 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<title>DGR SCUBA diving</title>
 	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Lemon" rel="stylesheet">
 
 </head>
 
 
 <body>
-<div id="container">
-		<?php include 'header.html';?>
-		<?php include 'navigation.html';?>
+	<?php include 'header.html';?>
+	<?php include 'navigation.html';?>
+
+<section class="top-page">
+	<img src="" alt="21">
+	<h1>Dive with Dgr</h1>
+</section>
+
+<section class="scrolling-text">
+
+</section>
+
+<section class="main-part">
+	<div class="main-left">
+About us
+	</div>
+	<div class="main-right">
+			<div class="main-right-top">
+Courses
+			</div>
+			<div class="main-right-bottom">
+Dive Sites
+			</div>
+	</div>
+</section>
+<section class="social-media">
+	<div class="social-left">
+	</div>
+	<div class="social-right">
+	</div>
+</section>
 
 
-		<section>
 
-			<article>
-				<header>
-					<h2>Article title</h2>
-					<p>Posted on <time datetime="2009-09-04T16:31:24+02:00">September 4th 2009</time> by <a href="#">Writer</a> - <a href="#comments">6 comments</a></p>
-				</header>
-				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-			</article>
-
-			<article>
-				<header>
-					<h2>Article title</h2>
-					<p>Posted on <time datetime="2009-09-04T16:31:24+02:00">September 4th 2009</time> by <a href="#">Writer</a> - <a href="#comments">6 comments</a></p>
-				</header>
-				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-			</article>
-
-		</section>
-
-		<aside>
-			<h2>About section</h2>
-			<p>Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-		</aside>
-
-		<footer>
-			<p>Copyright 2009 Your name</p>
-		</footer>
-</div>
+<footer>
+	<p>Copyright 2009 Your name</p>
+</footer>
 	</body>
-
 
 </html>
