bf168d76bf2f37da24cbcf5b522403d783fe56de
Grichan <vladgrichan@gmail.com>
2017-01-19 10:48:01 +0200

    sdasdasd

--- en/index.php
+++ en/index.php
@@ -8,16 +8,26 @@
 	<title>DGR SCUBA diving</title>
 	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Lemon" rel="stylesheet">
-
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="../assets/js/nav-scroll.js"></script>
 </head>
 
 
 <body>
+
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
 	<?php include 'header.html';?>
 	<?php include 'navigation.html';?>
 
 <section class="top-page">
-	<img src="" alt="21">
 	<h1>Dive with Dgr</h1>
 </section>
 
@@ -27,31 +37,32 @@
 
 <section class="main-part">
 	<div class="main-left">
-	<a href="#"><img src="../assets/images/dgr-diving-cyptus.jpg" alt="12"></a>
+			<a class="image1" href="#"> </a>
+			<a class="button" href="#">More About Us </a>
+			</div>
 
-		</div>
 
-	</div>
+
+
 	<div class="main-right">
 			<div class="main-right-top">
-Courses
+Dive Sites
 			</div>
 			<div class="main-right-bottom">
-Dive Sites
+Courses
 			</div>
 	</div>
 </section>
 <section class="social-media">
 	<div class="social-left">
+
+		<div class="fb-page" data-href="https://www.facebook.com/DGRScubaDiving/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/DGRScubaDiving/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/DGRScubaDiving/">DGR Scuba Diving</a></blockquote></div>
+
 	</div>
 	<div class="social-right">
 	</div>
 </section>
-<section class="quick-gallery">
-	<img class="first" src="image-1.jpg" alt="" />
-    	<img src="image-2.jpg" alt="" />
-    	<img src="image-3.jpg" alt="" />
-</section>
+
 
 
 
